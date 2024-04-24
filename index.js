3f6b5b50c929117f82f1ccceda29f52556756c8a
HARUN360 <mdharun573192@gmail.com>
2024-04-24 18:19:05 +0600

    this is last commit

--- index.js
+++ index.js
@@ -30,9 +30,10 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect(); commmit -1
 
-    const coffeeCollaction = client.db('coffeeDB').collection('coffee')
+    const coffeeCollaction = client.db('coffeeDB').collection('coffee');
+    const userCollection = client.db('coffeeDB').collection('user');
 
     app.get('/coffee', async (req, res) => {
       const cursor = coffeeCollaction.find();
@@ -82,13 +83,45 @@ async function run() {
       res.send(result)
     })
 
+    // user related api:
+    app.get('/user', async(req, res) => {
+      const cursor = userCollection.find();
+      const users = await cursor.toArray();
+      res.send(users)
+    })
+
+    app.post('/user', async(req, res) => {
+      const user = req.body;
+      console.log(user);
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    })
+
+    app.patch('/user', async (req, res) => {
+      const user = req.body;
+      const filter = {email: user.email}
+      const updateDoc = {
+        $set: {
+          lastLoggedAt: user.lastLoggedAt
+        }
+      }
+      const result = await userCollection.updateOne(filter, updateDoc)
+      res.send(result);
+    })
+
+    app.delete('/user/:id', async(req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await userCollection.deleteOne(query);
+      res.send(result);
+    })
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });  comment-2
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    // await client.close();
+    // await client.close(); comment-3
   }
 }
 run().catch(console.dir);
